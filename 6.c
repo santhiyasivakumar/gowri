a9b651a85ffd70206dd12d31cd29c2f658908708
santhiyasivakumar <30791898+santhiyasivakumar@users.noreply.github.com>
2017-08-21 13:23:57 +0530

    Create 6.c

---
+++ 6.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+string string_5;
+
+string string_1;
+
+cin >> string_2;
+
+cin >> string_1;
+
+if (areIsomorphic(string_5, string_1))
+
+cout << "True";
+
+else
+
+cout << "False";
+
+return 0;
+
+}
