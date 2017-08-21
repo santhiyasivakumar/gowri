b79c3df62be0cb43579866dc17484c1fdf74384b
santhiyasivakumar <30791898+santhiyasivakumar@users.noreply.github.com>
2017-08-21 13:44:34 +0530

    Create a1.c

---
+++ a1.c
@@ -0,0 +1,32 @@
+
+class DayOffChecker
+{
+    public bool CheckDays(List<string> DaysOff)
+    {
+        string CurrentDay = DateTime.Now.DayOfWeek.ToString();
+        CurrentDay.ToLower();
+        foreach (string DayCheck in DaysOff)
+        {
+            DayCheck.ToLower();
+            if (CurrentDay == DayCheck)
+            {
+                return (true);
+            }
+        }
+        return (false);
+    }
+}
+
+
+
+class Program
+{
+    List<string> DaysOff = List<string>();
+    DaysOff.Add("Saturday");  // Add some values to our list.
+    DaysOff.Add("Sunday");
+    DayOffChecker CheckToday = new DayOffChecker();
+    if(CheckToday.CheckDays(DaysOff))
+    {
+        Console.WriteLine("You're Off Today!!!");
+    }
+}
